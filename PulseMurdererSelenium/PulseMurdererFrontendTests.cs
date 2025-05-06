9bf7dd82dacc74ab47c254d792900f62a2f22698
Morten Chami Berner <mob001@edu.zealand.dk>
2025-05-06 12:29:16 +0200

    Show result test

--- PulseMurdererSelenium/PulseMurdererFrontendTests.cs
+++ PulseMurdererSelenium/PulseMurdererFrontendTests.cs
@@ -43,10 +43,14 @@ namespace PulseMurdererSelenium
             IWebElement buttonElement = _driver.FindElement(By.Id("button"));
             buttonElement.Click();
 
-            IWebElement showResultElement = _driver.FindElement(By.Id("showResult"));
-            string resultText = showResultElement.Text;
+            // Wait for the result element to be updated
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            IWebElement showResultElement = wait.Until(driver => driver.FindElement(By.Id("showResult")));
 
-            Assert.AreEqual(resultText, "The Murderer wins!");
+
+            // Verify the result text
+            string resultText = showResultElement.Text;
+            Assert.AreEqual("The Murderer wins!", resultText);
         }
     }
 }
