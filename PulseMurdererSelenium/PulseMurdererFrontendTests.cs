ef8abcf81eb898a0373f55b8a6449909c870f95f
Morten Chami Berner <mob001@edu.zealand.dk>
2025-05-06 13:05:46 +0200

    Two tests with two windows

--- PulseMurdererSelenium/PulseMurdererFrontendTests.cs
+++ PulseMurdererSelenium/PulseMurdererFrontendTests.cs
@@ -25,6 +25,7 @@ namespace PulseMurdererSelenium
             _driver?.Dispose();
         }
 
+
         [TestMethod]
 
         public void MurdererWinsTest()
@@ -48,6 +49,35 @@ namespace PulseMurdererSelenium
 
             string resultText = showResultElement.Text;
             Assert.AreEqual("The Murderer wins!", resultText);
+
+        }
+
+        [TestMethod]
+
+        public void PlayersWinsTest() 
+        {
+            //_driver?.Dispose();
+            IWebDriver? _driver = new FirefoxDriver(DriverDirectory);
+            string url = "http://127.0.0.1:5500/index.html";
+            _driver.Navigate().GoToUrl(url);
+            //_driver.Navigate().Refresh();
+
+            Assert.AreEqual("Game Status", _driver.Title);
+
+            IWebElement inputElementFirst = _driver.FindElement(By.Id("player1"));
+            inputElementFirst.SendKeys("2");
+
+            IWebElement inputElementSec = _driver.FindElement(By.Id("player2"));
+            inputElementSec.SendKeys("3");
+
+            IWebElement buttonElement = _driver.FindElement(By.Id("button"));
+            buttonElement.Click();
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+            IWebElement showResultElement = wait.Until(driver => driver.FindElement(By.Id("showResult")));
+
+            string resultText = showResultElement.Text;
+            Assert.AreEqual("The two players win!", resultText);
         }
     }
 }
