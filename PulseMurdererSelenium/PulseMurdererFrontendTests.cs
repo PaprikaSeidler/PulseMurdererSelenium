945d269e073fd23533b323fa10b070e39ab64788
Paprika Seidler <pas002@edu.zealand.dk>
2025-05-06 12:41:50 +0200

    Tests for US1 - PlayerPage

--- PulseMurdererSelenium/PulseMurdererFrontendTests.cs
+++ PulseMurdererSelenium/PulseMurdererFrontendTests.cs
@@ -8,14 +8,15 @@ namespace PulseMurdererSelenium
     public sealed class PulseMurdererFrontendTests
     {
         private static readonly string DriverDirectory = "c:\\WebDrivers";
+        //private static readonly string DriverDirectory = "C:\\web-drivers"; //Paprika
         private static IWebDriver? _driver;
 
         [ClassInitialize]
         public static void setup(TestContext context)
         {
             //_driver = new EdgeDriver(DriverDirectory);
-            //_driver = new ChromeDriver(DriverDirectory);
-            _driver = new FirefoxDriver(DriverDirectory);
+            _driver = new ChromeDriver(DriverDirectory);
+            //_driver = new FirefoxDriver(DriverDirectory);
         }
 
         [ClassCleanup]
@@ -32,6 +33,30 @@ namespace PulseMurdererSelenium
             _driver?.Navigate().GoToUrl(url);
 
             Assert.AreEqual("Game Status", _driver?.Title);
+        }
+
+        //PlayerPage
+        [TestMethod]
+        public void TestPlayerPageTitle()
+        {
+            string url = "http://127.0.0.1:5500/playerPage.html";
+            _driver?.Navigate().GoToUrl(url);
+            Assert.AreEqual("Player", _driver?.Title);
+        }
+
+        [TestMethod]
+        public void TestPlayerVoteButtons()
+        {
+            string url = "http://127.0.0.1:5500/playerPage.html";
+            _driver?.Navigate().GoToUrl(url);
+
+            var voterButton = _driver?.FindElements(By.Id("voterButton"));
+            voterButton[1].Click();
+            Assert.IsTrue(voterButton[1].GetAttribute("class").Contains("btn-success"));
+            Assert.AreEqual("John", voterButton[1].Text);
+
+            var voterButton2 = _driver?.FindElements(By.Id("voterButton"));
+            Assert.IsTrue(voterButton[0].GetAttribute("class").Contains("btn-primary"));
         }
     }
 }
